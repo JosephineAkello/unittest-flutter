cee80388ddb1c9199113df5af85009057e048183
mj <onyangomaureen95@gmail.com>
2019-10-24 22:23:38 +0300

    place other test cases

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,9 +2,19 @@ import 'package:test/test.dart';
 import 'package:unittests_flutter/counter.dart';
 
 void main(){
+  group('Counter', () {
+   test('value should start at O', (){
+    expect(Counter().value, 0);
+   });
   test('Counter value should be incremented', (){
    final counter =  Counter();
    counter.increment();
    expect(counter.value, 1);
   });
+  test('value should be decremented', (){
+  final counter = Counter();
+  counter.decrement();
+  expect(counter.value, -1);
+  });
+});
 }
